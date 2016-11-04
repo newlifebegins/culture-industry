d1d83fad24293c61b5faefbd2862d58755fde70b
weiwang <1612439458@qq.com>
2016-11-04 10:01:26 +0800

    fix

--- js/topMenu.js
+++ js/topMenu.js
@@ -1,14 +1,271 @@
 
-			$(function() {
-				$(".top_menuItemDiv").hide();
-				$(".top_menuItem").hover(function() {
-					$(this).find(".top_menuItemDiv").stop(true, true);
-					$(this).find(".top_menuItemDiv").slideDown();
-					$(this).find(".top_menuItemA").addClass("active")
+// top部分统一布局
+$(function(){
+	$("body").prepend(
+		'<div id="top">'+
+			'<div class="container clearfix">'+
+				'<ul class="left">'+
+					'<li><a href="/culture_industry/index.html">网站首页</a></li>'+
+					'<li><span> | </span></li>'+
+					'<li><a href="#">收藏本站</a></li>'+
+				'</ul>'+
+				'<ul class="right">'+
+					'<li><a href="/culture_industry/reg_login/login-personal.html">登录</a></li>'+
+					'<li><span> | </span></li>'+
+					'<li><a href="/culture_industry/reg_login/register-personal.html">个人注册</a></li>'+
+					'<li><span> | </span></li>'+
+					'<li><a href="/culture_industry/reg_login/register-company.html">企业注册</a></li>'+
+				'</ul>'+
+			'</div>'+
+		'</div>'+
+		'<div class="header">'+
+			'<div class="content">'+
+				'<a href="index.html" class="left logo"></a>'+
+				'<ul class="top_menuList right">'+
+					'<li class="top_menuItem">'+
+						'<a class="top_menuItemA" href="/culture_industry/index.html">首页</a>'+
+					'</li>'+
+					'<li class="top_menuItem">'+
+						'<a class="top_menuItemA" href="/culture_industry/news/index.html">资讯中心</a>'+
+						'<div class="top_menuItemDiv">'+
+							'<table class="table menu_Zx">'+
+								'<tbody>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/news/list.html">产业要闻</a>'+
+										'</td>'+
+										'<td>'+
+											'<a href="#">地市动态</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/news/pic.html">图片新闻</a>'+
+										'</td>'+
+										'<td>'+
+											'<a href="/culture_industry/news/level31.html">视频新闻</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/news/level32.html">热点专题</a>'+
+										'</td>'+
+										'<td>'+
+											'<a href="/culture_industry/news/notification.html">通知通告</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">企业推荐</a>'+
+										'</td>'+
+										'<td>'+
+											'<a href="#">产业研究</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/news/show.html">产业论坛</a>'+
+										'</td>'+
+										'<td>'+
+											'<a href="#">产业案例</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/news/level33.html">文化资源</a>'+
+										'</td>'+
+										'<td>'+
 
-				}, function() {
-					$(this).find(".top_menuItemDiv").stop(true, true);
-					$(this).find(".top_menuItemDiv").slideUp();
-					$(this).find(".top_menuItemA").removeClass("active")
-				});
-			})
+										'</td>'+
+									'</tr>'+
+								'</tbody>'+
+							'</table>'+
+						'</div>'+
+					'</li>'+
+					'<li class="top_menuItem">'+
+						'<a class="top_menuItemA" href="/culture_industry/policy/index.html">政策法规</a>'+
+						'<div class="top_menuItemDiv">'+
+							'<table class="table menu_Zx">'+
+								'<tbody>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">综合文化政策</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">文化产业政策</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">政策解读</a>'+
+										'</td>'+
+									'</tr>'+
+								'</tbody>'+
+							'</table>'+
+						'</div>'+
+					'</li>'+
+					'<li class="top_menuItem">'+
+						'<a class="top_menuItemA" href="/culture_industry/industrial/index.html">产业项目</a>'+
+						'<div class="top_menuItemDiv">'+
+							'<table class="table menu_Zx">'+
+								'<tbody>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">平台概况</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">重点项目推荐</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/industrial/detail3.html">产业项目发布</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">产业项目申报</a>'+
+										'</td>'+
+									'</tr>'+
+								'</tbody>'+
+							'</table>'+
+						'</div>'+
+					'</li>'+
+					'<li class="top_menuItem">'+
+						'<a class="top_menuItemA" href="/culture_industry/product_show/index.html">产品展示</a>'+
+						'<div class="top_menuItemDiv">'+
+							'<table class="table menu_Zx">'+
+								'<tbody>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/product_show/paper-cut.html">主题馆</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/product_show/area.html">地市馆</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/product_show/budd.html">旅游馆</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">国际馆</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/product_show/upload.html">产品发布</a>'+
+										'</td>'+
+									'</tr>'+
+								'</tbody>'+
+							'</table>'+
+						'</div>'+
+					'</li>'+
+					'<li class="top_menuItem">'+
+						'<a class="top_menuItemA" href="/culture_industry/finser/index.html">金融服务</a>'+
+						'<div class="top_menuItemDiv">'+
+							'<table class="table menu_Zx">'+
+								'<tbody>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/finser/agent.html">金融代理</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/finser/prod.html">信贷专区</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">文化银行</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">企业基金</a>'+
+										'</td>'+
+									'</tr>'+
+								'</tbody>'+
+							'</table>'+
+						'</div>'+
+					'</li>'+
+					'<li class="top_menuItem">'+
+						'<a class="top_menuItemA" href="/culture_industry/card/index.html">消费服务</a>'+
+						'<div class="top_menuItemDiv">'+
+							'<table class="table menu_Zx">'+
+								'<tbody>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">热门信息</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="/culture_industry/card/cardshow.html">消费攻略</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">商家联盟</a>'+
+										'</td>'+
+									'</tr>'+
+									'<tr>'+
+										'<td>'+
+											'<a href="#">联系我们</a>'+
+										'</td>'+
+									'</tr>'+
+								'</tbody>'+
+							'</table>'+
+						'</div>'+
+					'</li>'+
+				'</ul>'+
+
+			'</div>'+
+		'</div>'
+		).append(
+		'<div id="footer">'+
+    		'<div class="container">'+
+        		'<ul class="clearfix">'+
+            		'<li><a href="#">关于我们</a></li><span>|</span>'+
+            		'<li><a href="#">联系我们</a></li><span>|</span>'+
+            		'<li><a href="#">免费申明</a></li><span>|</span>'+
+            		'<li><a href="#">广告服务</a></li><span>|</span>'+
+            		'<li><a href="#">邮箱</a></li><span>|</span>'+
+            		'<li><a href="#">管理登录</a></li>'+
+        		'</ul>'+
+        		'<p>12321 | 12300 | 12818 | 网络违法犯罪举报网站 | 违法与不良信息举报中心</p>'+
+        		'<p>总访问量：1532846人次 总浏览量：5672745人次 开始统计日期：2009-09-19</p>'+
+        		'<p>ICP备案号：晋ICP备10201904号</p>'+
+        		'<p>主管：中共山西省委宣传部 山西省文化厅</p>'+
+        		'<p>主办：山西省文化产业发展中心</p>'+
+        		'<p>地址：中国-太原市桃园北路50号新闻大楼二层（邮编：030002）</p>'+
+        		'<p>电话：0351-4084258</p>'+
+        		'<p>邮编：sxwhcyw@163.com</p>'+
+    		'</div>'+
+		'</div>'
+		);
+
+});
+
+$(function() {
+	$(".top_menuItemDiv").hide();
+	$(".top_menuItem").hover(function() {
+		$(this).find(".top_menuItemDiv").stop(true, true);
+		$(this).find(".top_menuItemDiv").slideDown();
+		$(this).find(".top_menuItemA").addClass("active")
+
+	}, function() {
+		$(this).find(".top_menuItemDiv").stop(true, true);
+		$(this).find(".top_menuItemDiv").slideUp();
+		$(this).find(".top_menuItemA").removeClass("active")
+	});
+})
